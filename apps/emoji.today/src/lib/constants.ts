f671d0e2d3da8477f0cbe8795ed8907e9ff3600b
Daniel Colin James <danielcolinjames@gmail.com>
2025-05-30 16:00:41 -0300

    update values

--- apps/emoji.today/src/lib/constants.ts
+++ apps/emoji.today/src/lib/constants.ts
@@ -4,10 +4,10 @@ export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_FRAME_DESCRIPTION
 export const APP_PRIMARY_CATEGORY =
   process.env.NEXT_PUBLIC_FRAME_PRIMARY_CATEGORY
 export const APP_TAGS = process.env.NEXT_PUBLIC_FRAME_TAGS?.split(",")
-export const APP_ICON_URL = `${APP_URL}/icon.png`
-export const APP_OG_IMAGE_URL = `${APP_URL}/api/opengraph-image`
-export const APP_SPLASH_URL = `${APP_URL}/splash.png`
-export const APP_SPLASH_BACKGROUND_COLOR = "#f7f7f7"
+export const APP_ICON_URL = `${APP_URL}/icon-1024.png`
+export const APP_OG_IMAGE_URL = `${APP_URL}/og.png`
+export const APP_SPLASH_URL = `${APP_URL}/splash-200.png`
+export const APP_SPLASH_BACKGROUND_COLOR = "#050505"
 export const APP_BUTTON_TEXT = process.env.NEXT_PUBLIC_FRAME_BUTTON_TEXT
 export const APP_WEBHOOK_URL =
   process.env.NEYNAR_API_KEY && process.env.NEYNAR_CLIENT_ID
